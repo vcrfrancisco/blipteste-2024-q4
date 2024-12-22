0e3b210d8252c41b6c54d331bce66b60f81a597b
vcrfrancisco <vcrfrancisco@sga.pucminas.br>
2024-12-22 01:16:03 -0300

    feat: must get only the c# repos

--- src/controllers/get-takenet-git-repos/index.js
+++ src/controllers/get-takenet-git-repos/index.js
@@ -1,19 +1,20 @@
 import requestTakenetGitRepos from "./requests/takenet-git-repos.js"
 
 const getTakenetRepos = async () => {
-  const repos = await requestTakenetGitRepos('takenet');
-  const reposInfo = repos.map(
+  const repos = await requestTakenetGitRepos('takenet')
+  const oldest_five_csharp_repos = repos.filter(repo => repo.language === 'C#').slice(0,5)
+  const repos_info_formatted = oldest_five_csharp_repos.map(
     ({ name, description, url, created_at }) =>
     (
       {
         name,
         description,
         url,
-        created_at: new Date(created_at).toLocaleDateString('pt-BR')
+        created_at: new Date(created_at).toLocaleDateString('pt-BR'),
       }
     )
   )
-  return reposInfo;
+  return repos_info_formatted
 }
 
 export default getTakenetRepos;
--- src/controllers/get-takenet-git-repos/requests/takenet-git-repos.js
+++ src/controllers/get-takenet-git-repos/requests/takenet-git-repos.js
@@ -4,20 +4,20 @@ const github_token = process.env.GITHUB_TOKEN //TODO: mudar esse trem pra variá
 
 export default async function requestTakenetGitRepos() {
   const response = await axios({
-   method: "GET",
-   url: "/orgs/takenet/repos",
-   baseURL: "https://api.github.com",
-   params: {
-    sort: "created",
-    direction: "asc",
-    page: 1,
-    per_page: 5,
-  },
-   headers:{
-        Authorization: `Bearer ${github_token}`,
-        "X-GitHub-Api-Version": "2022-11-28",
-   },
-   timeout: 15000,
-})
+    method: "GET",
+    url: "/orgs/takenet/repos",
+    baseURL: "https://api.github.com",
+    params: {
+      sort: "created",
+      direction: "asc",
+      page: 1,
+      per_page: 10,
+    },
+    headers: {
+      Authorization: `Bearer ${github_token}`,
+      "X-GitHub-Api-Version": "2022-11-28",
+    },
+    timeout: 15000,
+  })
   return response.data
 }
